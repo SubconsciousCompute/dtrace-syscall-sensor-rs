f8c83cef3becfaa0383438a58a190ca52340ca7a
Cyber Phantom <inam123451@gmail.com>
2024-01-29 14:45:52 +0530

    Fix build.rs for windows

--- build.rs
+++ build.rs
@@ -6,14 +6,16 @@ use libbpf_cargo::SkeletonBuilder;
 #[cfg(target_family = "unix")]
 const SRC: &str = "src/linux/bpf/sensor.bpf.c";
 
+#[cfg(target_family = "unix")]
 fn main() {
-  if cfg!(target_family = "unix") {
-    let mut out = PathBuf::from(env::var_os("OUT_DIR").expect("No OUT_DIR env var set"));
-    out.push("sensor.skel.rs");
-    SkeletonBuilder::new()
-        .source(SRC)
-        .build_and_generate(&out)
-        .unwrap();
-    println!("cargo:rerun-if-changed={SRC}");
-  }
+  let mut out = PathBuf::from(env::var_os("OUT_DIR").expect("No OUT_DIR env var set"));
+  out.push("sensor.skel.rs");
+  SkeletonBuilder::new()
+      .source(SRC)
+      .build_and_generate(&out)
+      .unwrap();
+  println!("cargo:rerun-if-changed={SRC}");
 }
+
+#[cfg(target_family = "windows")]
+fn main() {}
