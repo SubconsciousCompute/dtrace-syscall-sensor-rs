f9f64108d958ff907e2c6f969d23c0d57af31df9
Cyber Phantom <inam123451@gmail.com>
2024-01-29 12:47:14 +0530

    Define Sensor trait

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::sync::mpsc::Receiver;
+
+mod utils;
+use utils::Record;
 #[cfg(target_family = "unix")]
 mod linux;
 
 #[cfg(target_family = "windows")]
 mod windows;
+
+pub trait Sensor {
+    fn get_process_tracker(&self) -> Receiver<Record>;
+    fn start(&self);
+    fn stop(&self);
+}
 
 fn main() {
     
